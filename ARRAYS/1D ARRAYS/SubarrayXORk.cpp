664206905b44a76cf117c368e05cd6ae6d6defca
amancreatives <kukrejaaman2006@gmail.com>
2025-07-05 17:00:35 +0530

    XOR K

---
+++ ARRAYS/1D ARRAYS/SubarrayXORk.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int subArrayWithXorK(vector<int>& nums, int k){
+    int n = nums.size();
+    map<int, int> mp;
+    int xr = 0;
+    int ctr = 0;
+    mp[xr]++;
+    for(int i=0; i<n; i++){
+        xr = xr^k;
+        int x = xr^k;
+        ctr += mp[x];
+        mp[xr]++;
+    }
+    return ctr;
+}
+
+
+int main(){
+    vector<int> nums = {1, 2, 3, 4, 5};
+    int k = 3;
+    cout << "Number of subarrays with XOR equal to " << k << ": " << subArrayWithXorK(nums, k) << endl;
+    return 0;
+}
+
+// TC --> O(n)
+// SC --> O(1)
