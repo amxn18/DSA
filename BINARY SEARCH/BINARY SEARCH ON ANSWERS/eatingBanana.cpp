47db98a57ad5b4fa043d8128a25509dc7aa22a9f
amancreatives <kukrejaaman2006@gmail.com>
2025-08-09 12:20:27 +0530

    Changes

--- BINARY SEARCH/BINARY SEARCH ON ANSWERS/eatingBanana.cpp
+++ BINARY SEARCH/BINARY SEARCH ON ANSWERS/eatingBanana.cpp
@@ -1,28 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
+    long long reqHrs(vector<int>& piles, int hourly) {
+        int n = piles.size();
+        long long totalHrs = 0; // long long to avoid overflow
+        for (int i = 0; i < n; i++) {
+            totalHrs += ((long long)piles[i] + hourly - 1) / hourly; 
+        }
+        return totalHrs;
+    }
+
     int minEatingSpeed(vector<int>& piles, int h) {
-        int left = 1;
-        int right = *max_element(piles.begin(), piles.end());
-        int result = right;
+        int low = 1;
+        int high = *max_element(piles.begin(), piles.end());
+        int ans = INT_MAX;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            long long totalHours = 0;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long long totalhrs = reqHrs(piles, mid);
 
-            for (int pile : piles) {
-                totalHours += (pile + mid - 1) / mid;  // Ceil division without using floating point
-            }
-
-            if (totalHours <= h) {
-                result = mid;          // Try smaller k
-                right = mid - 1;
+            if (totalhrs <= h) {
+                ans = mid;
+                high = mid - 1;
             } else {
-                left = mid + 1;        // Try larger k
+                low = mid + 1;
             }
         }
-        return result;
+        return ans;
     }
 };
 
