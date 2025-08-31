c1a57d6ffc0799b07abf8054cea73ea75422de7e
amancreatives <kukrejaaman2006@gmail.com>
2025-08-31 13:33:41 +0530

    Better approach

--- ARRAYS/1D ARRAYS/lenOfLongestSubArrayWithSumKPositive.cpp
+++ ARRAYS/1D ARRAYS/lenOfLongestSubArrayWithSumKPositive.cpp
@@ -1,6 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Better approach using Hashing and optimal approach for both positive and negative numbers
+// TC = O(n)
+// SC = O(n)
+class Solution {
+  public:
+    int longestSubarray(vector<int>& arr, int k) {
+        int n = arr.size();
+        unordered_map<long, long> mp;
+        long long sum = 0;
+        int len = 0;
+        for(int i =0; i<n; i++){
+            sum += arr[i];
+            if(sum == k) len = max(len, i+1);
+            long long rem = sum - k;
+            if(mp.find(rem) !=mp.end()){
+                int length = i - mp[rem];
+                len = max(len, length);
+            }
+            if(mp.find(sum) == mp.end()) mp[sum] = i;
+        }
+        return len;
+    }
+};
+
+
 int longestSubarray(vector<int>&nums, int k){
     int n = nums.size();
     map<long, long> mp;
