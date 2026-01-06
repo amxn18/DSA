8c0f67b3cd17fa5c58fed5f3c7b2dde3f5d575f2
Aman Kukreja <kukrejaaman2006@gmail.com>
2026-01-06 17:27:34 +0530

    MST using Kruskal

---
+++ GRAPHS/Problems/MSTUsingKruskal.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> parent, rank;
+
+    int find(int x) {
+        if (parent[x] == x)
+            return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    void unionSet(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+
+        if (px == py) return;
+
+        if (rank[px] < rank[py]) {
+            parent[px] = py;
+        }
+        else if (rank[px] > rank[py]) {
+            parent[py] = px;
+        }
+        else {
+            parent[py] = px;
+            rank[px]++;
+        }
+    }
+
+    int Kruskal(int V, vector<vector<int>>& edges) {
+        parent.resize(V);
+        rank.resize(V, 0);
+
+        for (int i = 0; i < V; i++)
+            parent[i] = i;
+
+        int sum = 0;
+
+        for (auto &e : edges) {
+            int u = e[0];
+            int v = e[1];
+            int wt = e[2];
+
+            if (find(u) != find(v)) {
+                unionSet(u, v);
+                sum += wt;
+            }
+        }
+        return sum;
+    }
+
+    int spanningTree(int V, vector<vector<int>>& edges) {
+
+        sort(edges.begin(), edges.end(),
+             [](vector<int>& a, vector<int>& b) {
+                 return a[2] < b[2];
+             });
+
+        return Kruskal(V, edges);
+    }
+};
+
+// TC 
