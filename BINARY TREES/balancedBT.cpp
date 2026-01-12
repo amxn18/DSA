2ca1ab145002b32fb040f5ea2f0b54547e3c93ea
Aman Kukreja <kukrejaaman2006@gmail.com>
2026-01-12 21:12:14 +0530

    LC 110 Brute

--- BINARY TREES/balancedBT.cpp
+++ BINARY TREES/balancedBT.cpp
@@ -11,6 +11,30 @@ struct TreeNode {
      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
+
+// Brute TC O(N^2) SC O(H)
+class Solution {
+public:
+    int findHt(TreeNode* node){
+        if(node == NULL) return 0;
+        int lh = findHt(node->left);
+        int rh = findHt(node->right);
+        return 1 + max(lh, rh);
+    }
+    bool isBalanced(TreeNode* root) {
+        if(root == NULL) return true;
+        int lh = findHt(root->left);
+        int rh = findHt(root->right);
+
+        if(abs(lh-rh) > 1) return false;
+        bool left = isBalanced(root->left);
+        bool right = isBalanced(root->right);
+        if(!left || !right) return false;
+        return true;
+    }
+};
+
+// Optimal TC O(N) SC O(H)
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
