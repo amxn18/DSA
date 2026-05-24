5fa7f008279f1d10298df545d1d030f835a7cf26
Aman Kukreja <kukrejaaman2006@gmail.com>
2026-05-24 14:33:59 +0530

    LC 152

--- ARRAYS/1D ARRAYS/maxProductSubarray.cpp
+++ ARRAYS/1D ARRAYS/maxProductSubarray.cpp
@@ -18,3 +18,26 @@ public:
         return (int)(maxProd);
     }
 };
+
+// Method 2: Kadane's Algorithm: O(n)
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int maxProd = nums[0];
+        int currMax = nums[0];
+        int currMin = nums[0];
+
+        for(int i=1; i<n; i++){
+            int num = nums[i];
+            int tempMax = max({num, currMax*num, currMin*num});
+            int tempMin = min({num, currMax*num, currMin*num});
+
+            currMax = tempMax;
+            currMin = tempMin;
+
+            maxProd = max(maxProd, currMax);
+        }
+        return maxProd;
+    }
+};
