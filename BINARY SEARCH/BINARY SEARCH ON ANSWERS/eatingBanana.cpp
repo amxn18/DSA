c2e4b20048eef25b46822539d6ef1bfa492a346e
Aman Kukreja <kukrejaaman2006@gmail.com>
2026-06-11 11:27:59 +0530

    875

--- BINARY SEARCH/BINARY SEARCH ON ANSWERS/eatingBanana.cpp
+++ BINARY SEARCH/BINARY SEARCH ON ANSWERS/eatingBanana.cpp
@@ -1,39 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include <bits/stdc++.h>
-using namespace std;
+
+// LC 875. Koko Eating Bananas
+// TC = O(nlogm) where n is the number of piles and m is the maximum pile size
 
 class Solution {
 public:
-    long long reqHrs(vector<int>& piles, int hourly) {
-        int n = piles.size();
-        long long totalHrs = 0; // long long to avoid overflow
-        for (int i = 0; i < n; i++) {
-            totalHrs += ((long long)piles[i] + hourly - 1) / hourly; 
+    long long calculateTime(vector<int>& piles, int hourly){
+        long long total = 0;
+        for(int i=0; i<piles.size(); i++){
+            total += ceil((double)piles[i]/hourly);
         }
-        return totalHrs;
+        return total;
     }
-
     int minEatingSpeed(vector<int>& piles, int h) {
         int low = 1;
         int high = *max_element(piles.begin(), piles.end());
-        int ans = INT_MAX;
+        int ans = high;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            long long totalhrs = reqHrs(piles, mid);
-
-            if (totalhrs <= h) {
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            long long hours = calculateTime(piles, mid);
+            if(hours <= h){
                 ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+                high = mid-1;
             }
+            else low = mid+1;
         }
         return ans;
     }
 };
-
-// LC 875
-// TC: O(n log m) where n is the number of piles and m is the maximum pile size
-// SC: O(1) since we are using constant space
