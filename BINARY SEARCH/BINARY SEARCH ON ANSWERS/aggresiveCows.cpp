40ae6c2e8eabb3828beacdaee2db8bc96cd850da
Aman Kukreja <kukrejaaman2006@gmail.com>
2026-06-13 14:09:26 +0530

    .

--- BINARY SEARCH/BINARY SEARCH ON ANSWERS/aggresiveCows.cpp
+++ BINARY SEARCH/BINARY SEARCH ON ANSWERS/aggresiveCows.cpp
@@ -1,36 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+// Aggressive Cows
+// TC: O(NlogN) + O(logN * N) = O(NlogN)
 class Solution {
   public:
-    int isPossible(vector<int>& stalls, int dist, int k){
-        int n = stalls.size();
-        int cnt = 1;
-        int lastCow = stalls[0];
-        for(int i=1; i<n; i++){
-            if(stalls[i] - lastCow >= dist){
-                cnt++;
-                lastCow = stalls[i];
-            }
-            if(cnt >= k) return true;
+    bool isPossible(vector<int>& nums, int dist, int k){
+        int countCows = 1;
+        int lastCow = nums[0];
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i] - lastCow >= dist){
+                countCows++;
+                lastCow = nums[i];
+            } 
+            if(countCows >= k) return true;
         }
         return false;
     }
     int aggressiveCows(vector<int> &stalls, int k) {
         int n = stalls.size();
+        sort(stalls.begin(), stalls.end());
+        int ans = -1;
         int low = 1;
         int high = stalls[n-1] - stalls[0];
-        sort(stalls.begin(), stalls.end());
         while(low <= high){
-            int mid = low + (high -low)/2;
-            if(isPossible(stalls, mid, k) == true){
-                low = mid+1;
-            } else high = mid-1;
+            int mid = low + (high-low)/2;
+            if(isPossible(stalls, mid, k)){
+                ans = mid;
+                low = mid+1; // need max 
+            }
+            else high = mid-1;
         }
-        return high;
-        
+        return ans;
     }
 };
-
-
-// Tc : O(n log m) m --> maximum distance between first and last stall
-// Sc : O(1)
--- BINARY SEARCH/BINARY SEARCH ON ANSWERS/bookAllocation.cpp
+++ BINARY SEARCH/BINARY SEARCH ON ANSWERS/bookAllocation.cpp
@@ -1,32 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+// Book Allocation Problem
+// TC: O(Log(sum-max+1) * N) = O(NlogN)
 class Solution {
   public:
-    int solve(vector<int>& arr, int pages){
-        int n = arr.size();
-        long long pagesPerStudent = 0;
+    int findStudents(vector<int>& nums, int minPages){
         int students = 1;
-        for(int i=0; i<n; i++){
-            if(pagesPerStudent + arr[i] <= pages){
-                pagesPerStudent += arr[i];
-            } else{
+        int studentsPage = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(studentsPage + nums[i] <= minPages) studentsPage += nums[i];
+            else{
                 students++;
-                pagesPerStudent = arr[i];
+                studentsPage = nums[i];
             }
         }
         return students;
     }
     int findPages(vector<int> &arr, int k) {
         int n = arr.size();
-        if(k > n) return -1; // No of students is more then no of books
+        if(k > n) return -1;
         int low = *max_element(arr.begin(), arr.end());
         int high = accumulate(arr.begin(), arr.end(), 0);
         while(low <= high){
-            int mid =  low + (high-low)/2;
-            int noOfStudents = solve(arr, mid);
-            if(noOfStudents > k) low = mid+1;
-            else high = low+1;
+            int mid = low + (high-low)/2;
+            int students = findStudents(arr, mid);
+            if(students > k) low = mid+1;
+            else high = mid-1;
         }
         return low;
+        
     }
 };
