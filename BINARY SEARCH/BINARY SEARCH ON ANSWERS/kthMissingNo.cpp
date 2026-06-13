e51412512490c6a8f617737681a299e8c9d2cef3
Aman Kukreja <kukrejaaman2006@gmail.com>
2026-06-13 11:39:08 +0530

    LC 1539

--- BINARY SEARCH/BINARY SEARCH ON ANSWERS/kthMissingNo.cpp
+++ BINARY SEARCH/BINARY SEARCH ON ANSWERS/kthMissingNo.cpp
@@ -1,5 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+
+// LC` 1539. Kth Missing Positive Number
+// M1: O(N)
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int n = arr.size();
+        int num = 1;
+        int i = 0;
+        while(i < n && k > 0){
+            if(arr[i] == num) i++;
+            else k--;
+            num++;
+        }
+        while(k--) num++; // {1,2,3} k = 2 -> OP = num = 5
+        return num-1;
+    }
+};
+
+// M2 O(N)
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int n = arr.size();
+        for(int i=0; i<n; i++){
+            if(arr[i] <= k)k++;
+            else break;
+        }
+        return k;
+    }
+};
+// M3 O(log N)
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
@@ -16,6 +49,4 @@ public:
     }
 };
 
-// LC 1539
-// TC: O(log n)
-// SC: O(1)
+\
