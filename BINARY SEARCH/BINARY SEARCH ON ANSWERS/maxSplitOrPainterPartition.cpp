fa3980982187810c3a53d26b21e8f1585d642ef9
Aman Kukreja <kukrejaaman2006@gmail.com>
2026-06-13 14:29:22 +0530

    LC 410

--- BINARY SEARCH/BINARY SEARCH ON ANSWERS/maxSplitOrPainterPartition.cpp
+++ BINARY SEARCH/BINARY SEARCH ON ANSWERS/maxSplitOrPainterPartition.cpp
@@ -1,49 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// LC 410. Split Array Largest Sum
+// TC: O(n log m) where m is the sum of elements in nums
 class Solution {
 public:
-    // Helper function to count how many subarrays are formed if max subarray sum is <= mid
-    int solve(vector<int>& nums, int mid) {
-        int count = 1; // At least one subarray
+    int findSplits(vector<int>& arr, int s){
+        int splits = 1;
         int sum = 0;
-        for (int num : nums) {
-            if (sum + num <= mid) {
-                sum += num;
-            } else {
-                count++;
-                sum = num;
+        for(int i=0; i<arr.size(); i++){
+            if(sum + arr[i] <= s) sum += arr[i];
+            else{
+                splits++;
+                sum = arr[i];
             }
         }
-        return count;
+        return splits;
     }
-
-    int findMaxSubarray(vector<int>& nums, int k) {
+    int splitArray(vector<int>& nums, int k) {
         int n = nums.size();
-        if (k > n) return -1; // If splits > elements, invalid
-
-        int low = *max_element(nums.begin(), nums.end()); // Minimum max subarray sum possible
-        int high = accumulate(nums.begin(), nums.end(), 0); // Maximum max subarray sum possible
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            int splits = solve(nums, mid);
-            if (splits > k) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+        if(k > n) return -1;
+        int low = *max_element(nums.begin(), nums.end());
+        int high = accumulate(nums.begin(), nums.end(), 0);
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            int splits = findSplits(nums, mid);
+            if(splits > k) low = mid+1;
+            else high = mid-1;
         }
         return low;
     }
-
-    int splitArray(vector<int>& nums, int k) {
-        return findMaxSubarray(nums, k);
-    }
 };
-
-
-// Same As Book Allocation Problem
-// TC: O(n log m) where m is the sum of elements in nums
-// SC: O(1)
-// LC 410
