de762e51566d5b861af08cdd21af25b8c4527fb2
Aman Kukreja <kukrejaaman2006@gmail.com>
2026-06-15 11:48:11 +0530

    LC 547

--- GRAPHS/Problems/noOfProviences.cpp
+++ GRAPHS/Problems/noOfProviences.cpp
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+// LC 547. Number of Provinces
+// M1: Using adj matrix -> TC: O(V^2) and SC: O(V) for recursion stack & visited array.
 class Solution {
 public:
     void DFS(int u, vector<vector<int>>& adj, vector<bool>& vis, int V){
@@ -23,4 +26,31 @@ public:
     }
 };
 
-// TC O(V^2)
+// Using adj list -> TC : O(V^2) {adj list} + O(V+E) {DFS}, SC : O (V+ E) {adj list} + O(V) {visited} + O(V) {rec stack}.
+class Solution {
+public:
+    void DFS(int u, vector<vector<int>>& adj, vector<bool>& visited){
+        visited[u] = true;
+        for(auto &v: adj[u]){
+            if(!visited[v]) DFS(v, adj, visited);
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int V = isConnected.size();
+        vector<vector<int>> adj(V);
+        for(int i=0; i<V; i++){
+            for(int j=0; j<V; j++){
+                if(i != j && isConnected[i][j] == 1) adj[i].push_back(j);
+            }
+        }
+        vector<bool> visited(V, false);
+        int ctr = 0;
+        for(int v=0; v<V; v++){
+            if(!visited[v]){
+                ctr++;
+                DFS(v, adj, visited);
+            }
+        }
+        return ctr;
+    }
+};
