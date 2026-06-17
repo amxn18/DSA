1adc42b3ae7e540b1fd35a686fc16e1fbe6fdca9
Aman Kukreja <kukrejaaman2006@gmail.com>
2026-06-17 12:29:57 +0530

    LC 126

---
+++ GRAPHS/Problems/BFS & DFS/wordLadder2.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// LC 126: Word Ladder II
+// Method 1
+class Solution {
+public:
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> st(wordList.begin(), wordList.end());
+        queue<vector<string>> q;
+        q.push({beginWord});
+        vector<string> usedStringOnLevel;
+        usedStringOnLevel.push_back(beginWord);
+        int level = 0;
+        vector<vector<string>> ans;
+        if(st.find(endWord) == st.end()) return ans;
+        while(!q.empty()){
+            auto vec = q.front();
+            q.pop();
+            // erase all the words used in previous level to transform
+            if(vec.size() > level){ // New Level
+                level++;
+                for(auto it: usedStringOnLevel){
+                    st.erase(it);
+                }
+            }
+            string word = vec.back(); // last word
+            if(word == endWord){
+                if(ans.size() == 0) ans.push_back(vec); // First sequence 
+                else if(ans[0].size() == vec.size()) ans.push_back(vec);
+            }
+            for(int i=0; i<word.size(); i++){
+                char original = word[i];
+                for(char ch='a'; ch <= 'z'; ch++){
+                    if(ch == original) continue;
+                    word[i] = ch;
+                    if(st.find(word) != st.end()){
+                        vec.push_back(word); // append the word in the sequence
+                        q.push(vec); // push the new sequence
+                        usedStringOnLevel.push_back(word); // mark as visited on level
+                        vec.pop_back(); // remove the added word to check another sequence
+                    }
+                }
+                word[i] = original;
+            }
+        }
+        return ans;
+    }
+};
