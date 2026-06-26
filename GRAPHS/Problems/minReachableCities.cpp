c31a66b7220c0b8efb347e47cf7ac5b2e52af51b
Aman Kukreja <kukrejaaman2006@gmail.com>
2026-06-26 21:56:46 +0530

    LC 1334

--- GRAPHS/Problems/minReachableCities.cpp
+++ GRAPHS/Problems/minReachableCities.cpp
@@ -69,3 +69,44 @@ public:
     }
 };
 
+// Using Floyd Warshall TC O(n³)   SC O(n²)
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> dist(n, vector<int>(n, 1e9));
+        // Src -> Src : Dist = 0
+        for(int i=0; i<n; i++){
+            dist[i][i] = 0;
+        }
+        for(auto edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+            // Bidirectional 
+            dist[u][v] = wt;
+            dist[v][u] = wt;
+        }
+        for(int via=0; via<n; via++){
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                    dist[i][j] = min(dist[i][j], dist[i][via] + dist[via][j]);
+                }
+            }
+        }
+        int city = -1;
+        int minCount = INT_MAX;
+        for(int i=0; i<n; i++){
+            int ctr = 0;
+            for(int j=0; j<n; j++){
+                if(i != j && dist[i][j] <= distanceThreshold) ctr++;
+            }
+            if(ctr <= minCount){
+                minCount = ctr;
+                city = i;
+            }
+        }
+        return city;
+
+    }
+};
+
