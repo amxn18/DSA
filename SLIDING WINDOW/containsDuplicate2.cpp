82405153aa636fffd520a83acf03f09d1f2da738
Aman Kukreja <kukrejaaman2006@gmail.com>
2026-08-19 23:19:18 +0530

    LC 219

---
+++ SLIDING WINDOW/containsDuplicate2.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// LC 219. Contains Duplicate II
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_set<int> st;
+        int i = 0;
+        int j = 0;
+        while(j<n){
+            if(abs(i-j) > k){
+                st.erase(nums[i]);
+                i++;
+            }
+            if(abs(i-j) <= k && st.find(nums[j]) != st.end()) return true;
+            st.insert(nums[j]);
+            j++;
+        }   
+        return false;
+    }
+};
